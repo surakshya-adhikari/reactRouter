6ba3451937f1fcfc1ed535053340d0587f836c7a
surakshya-adhikar <surakshyaadhikari09@gmail.com>
2025-02-24 19:15:48 +0545

    Set up React Router with createBrowserRouter for navigation between Home, AboutUs, and Contact pages using a Layout component as the parent route

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,37 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './Home/Home.jsx'
+import Contact from './Contact/Contact.jsx'
+import AboutUs from './AboutUs/AboutUs.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: "",
+        element: <Home/>
+      },
+      {
+        path: "about",
+        element: <AboutUs/>
+      },
+      {
+        path: "contact",
+        element:<Contact/>
+      }
+    ]
+  }
+])
+
+ 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+   <RouterProvider router={router}/>
   </StrictMode>,
 )
